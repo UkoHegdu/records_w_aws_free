e02cc74c4caa05c88a9f57421d55b20d188492fe
UkoHegdu <fantomass@gmail.com>
2025-04-30 11:47:38 +0300

    testing

--- init.sql
+++ init.sql
@@ -1,7 +1,17 @@
-CREATE TABLE alerts (
-    id SERIAL PRIMARY KEY,
-    username TEXT NOT NULL,
-    email TEXT NOT NULL,
-	userid TEXT NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
+create table alerts (
+   id         serial primary key,
+   username   text not null,
+   email      text not null,
+   uuserid    text not null,
+   created_at timestamp default now()
 );
+
+insert into alerts (
+   username,
+   email,
+   uuserid,
+   created_at
+) values ( 'teh_macho',
+           'fantomass@gmail.com',
+           '444',
+           now() );
